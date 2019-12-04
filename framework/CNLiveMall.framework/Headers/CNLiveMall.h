ee2949c8fcce50b3b452c8f51b573f70116394c3
caikaixuan <feng520ckx@gmail.com>
2019-12-04 14:59:48 +0800

    提交静态库

---
+++ framework/CNLiveMall.framework/Headers/CNLiveMall.h
@@ -0,0 +1,20 @@
+//
+//  CNLiveMall.h
+//  CNLiveMall
+//
+//  Created by caikaixuan on 2019/11/29.
+//  Copyright © 2019 caikaixuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CNLiveMall.
+FOUNDATION_EXPORT double CNLiveMallVersionNumber;
+
+//! Project version string for CNLiveMall.
+FOUNDATION_EXPORT const unsigned char CNLiveMallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CNLiveMall/PublicHeader.h>
+#import<CNLiveMall/HTTabBarController.h>
+
+
